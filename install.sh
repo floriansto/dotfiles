e6d1f964249afde894d2bd201c55aecb5e40b8dc
Florian Stockburger <93-orangen@web.de>
2018-09-17 23:58:06 +0200

    Fix whitespaces

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ cd "${BASE_DIR}"
 BASE_DIR="${PWD}"
 
 function log {
-	printf '\e[1;33m%*s\e[0m' "${COLUMNS}" '' | tr ' ' -
- 	printf '\r\e[1;33m-- \e[1;32m%s \e[0m\n' "$*"
+    printf '\e[1;33m%*s\e[0m' "$COLUMNS" '' | tr ' ' -
+    printf '\r\e[1;33m-- \e[1;32m%s \e[0m\n' "$*"
 }
 
 function ln_os {
@@ -31,19 +31,19 @@ function install_zsh {
     git pull && git submodule update --init --recursive
     cd "${BASE_DIR}/zsh"
     for file in oh-my-zsh zshrc; do
-		[ -L "${HOME}/.${file}" ] && rm -v "${HOME}/.${file}"
-		ln_os "`readlink -f ${file}`" "${HOME}/.${file}"
-	done
+        [ -L "${HOME}/.${file}" ] && rm -v "${HOME}/.${file}"
+        ln_os "`readlink -f ${file}`" "${HOME}/.${file}"
+    done
     backup=""
     [ $OSTYPE != "linux-android" ] && backup="--backup"
     [ -f ~/.envConf ] && [ "`head -n1 ~/.envConf`"="# zsh config file (version: ${VERSION})" ] && source ~/.envConf || mv -vi $backup  ~/.envConf ~/.envConf_`date +%F_%H%M%S`
     [ -z "$ZSH_THEME" ] && read -p "please enter the zsh theme you wanna use(to use 'bira' just hit enter): " ZSH_THEME
-	[ -z "$ZSH_THEME" ] && ZSH_THEME="bira"
+    [ -z "$ZSH_THEME" ] && ZSH_THEME="bira"
 
     local ZSH_CUSTOM="${BASE_DIR}/zsh/custom"
     echo "# zsh config file (version: $VERSION)" >~/.envConf
-	echo "ZSH_CUSTOM=\"$ZSH_CUSTOM\"" >>~/.envConf
-	echo "ZSH_THEME=\"$ZSH_THEME\"" >>~/.envConf
+    echo "ZSH_CUSTOM=\"$ZSH_CUSTOM\"" >>~/.envConf
+    echo "ZSH_THEME=\"$ZSH_THEME\"" >>~/.envConf
     echo "ZSH_DISABLE_COMPFIX=\"true\"" >>~/.envConf
 
 }
@@ -64,18 +64,18 @@ function install_vim {
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     local TMP_VIMRC=`mktemp`
     echo "set nocompatible" >$TMP_VIMRC
-	echo "filetype off" >>$TMP_VIMRC
-	echo "call plug#begin('~/.vim/bundle')" >>$TMP_VIMRC
-	grep "^Plug " "${BASE_DIR}/vim/vimrc" >>$TMP_VIMRC
-	echo "call plug#end()" >>$TMP_VIMRC
-	vim -u "${TMP_VIMRC}" +PlugUpgrade +PlugInstall +PlugUpdate +qall
-	rm -v $TMP_VIMRC
+    echo "filetype off" >>$TMP_VIMRC
+    echo "call plug#begin('~/.vim/bundle')" >>$TMP_VIMRC
+    grep "^Plug " "${BASE_DIR}/vim/vimrc" >>$TMP_VIMRC
+    echo "call plug#end()" >>$TMP_VIMRC
+    vim -u "${TMP_VIMRC}" +PlugUpgrade +PlugInstall +PlugUpdate +qall
+    rm -v $TMP_VIMRC
 
 }
 
 for job in git zsh vim; do
-	log "installing configuration for '$job'"
-	install_${job}
-	cd "${BASE_DIR}"
+    log "installing configuration for '$job'"
+    install_${job}
+    cd "${BASE_DIR}"
 done
 log "Finish"
