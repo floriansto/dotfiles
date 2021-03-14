e55abaf734e82139f36595f4fe81ef55559c5966
Florian Stockburger <florian.stockburger@web.de>
2021-03-15 00:19:51 +0100

    Update i3 media controls

--- i3/lock.sh
+++ i3/lock.sh
@@ -2,7 +2,7 @@
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 [[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png ~/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
-dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+playerctl pause
 i3lock -i /tmp/screen.png
 rm /tmp/screen.png
 
