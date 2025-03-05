585c334770aa481a757898f7c650c09fedc0c23c
Florian Stockburger <stockburger.florian@gmail.com>
2025-03-06 00:11:23 +0100

    Use hex color code for zsh auto suggestions

--- zsh/custom/zsh-autosuggestions.zsh
+++ zsh/custom/zsh-autosuggestions.zsh
@@ -32,4 +32,4 @@
 # Color to use when highlighting suggestion
 # Uses format of `region_highlight`
 # More info: http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Zle-Widgets
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=3'
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#ffd700"
